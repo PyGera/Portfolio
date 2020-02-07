db87510577ffbb612326369cb7ab6662d9467f48
Federico Gerardi <federico.gerardi03@gmail.com>
2020-02-07 22:53:28 +0100

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
+import profileImg from "./pp.jpeg";
 import './App.css';
+
+const name = "Federico Gerardi";
+const profession = "Software Developer";
+
+
+function ProfileImg() {
+    return (
+        <img alt="profileImg" src={profileImg} className="profileImg" />
+    );
+}
+
+function Name() {
+    return (
+        <div className="name">{name}</div>
+    );
+}
+
+function Profession() {
+    return (
+        <div className="profession">{profession}</div>
+    );
+}
+
+function Button(props) {
+    return (
+        <button className="btn" onClick={props.onClick}>{props.value}</button>
+    );
+}
+
+function Landing() {
+  return (
+      <div className="landing">
+        <ProfileImg />
+        <Name />
+        <Profession />
+        <Button value="Progetti"/>
+        <br />
+        <Button value="Scarica il CV" />
+      </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Landing />
   );
 }
 
