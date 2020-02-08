9a3d90d47c2a063510bb4be41b6ca24f5aecda16
Federico Gerardi <federico.gerardi03@gmail.com>
2020-02-08 23:43:14 +0100

    Project Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import profileImg from "./pp.jpeg";
+import CanSatImg from "./cansat.png";
+import TrashBotImg from "./tb.jpeg";
+import GieriLangImg from "./gl.png";
 import './App.css';
 
 const name = "Federico Gerardi";
 const profession = "Software Developer";
 
+let page = 0;
 
 function ProfileImg() {
     return (
@@ -36,15 +41,23 @@ function Social(props) {
     );
 }
 
+function gotoProjects() {
+    ReactDOM.render(<Projects />, document.getElementById("root"))
+}
+
+function gotoLanding() {
+    ReactDOM.render(<Landing />, document.getElementById("root"))
+}
+
 //TODO: Create an array of objects with all socials, add every social in a JSX var and insert it in Landing
 
 function Landing() {
   return (
-      <div className="landing">
+      <div className="windows">
         <ProfileImg />
         <Name />
         <Profession />
-        <Button value="Progetti"/>
+        <Button onClick={gotoProjects} value="Progetti"/>
         <br />
         <Button value="Scarica il CV" />
         <br />
@@ -58,10 +71,40 @@ function Landing() {
   );
 }
 
-function App() {
-  return (
-    <Landing />
-  );
+function Project(props) {
+    return (
+        /*<table>
+            <tr>
+                <td class="td_img"><img className="projectImg" src={props.src} alt="projectImg" /></td>
+                <td className="projectWrites">
+                    <div className="projectTitle">{props.title}</div>
+                    <div>{props.description}</div>
+                </td>
+            </tr>
+        </table>*/
+        <div className="projectCard">
+            <div className="image-box">
+                <img src={props.src} alt="project image" className="projectImg" />
+            </div>
+            <div className="projectTitle">{props.title}</div>
+            <div className="projectDescription">{props.description}</div>
+            <div className="projectPeriod">{props.period}</div>
+        </div>
+
+    );
 }
 
-export default App;
+function Projects() {
+    return (
+        <div className="windows">
+            <i className="fas fa-arrow-left icons freccia" onClick={gotoLanding}></i>
+            <div className="title">Progetti</div>
+            <br />
+            <Project src={GieriLangImg} title="GieriLang" description="Personale" period="2019-Now"/>
+            <Project src={TrashBotImg} title="TrashBot" description="ITIS E. Fermi" period="2019-Now"/>
+            <Project src={CanSatImg} title="CanSat" description="ASI" period="2019"/>
+        </div>
+    );
+}
+
+export default Landing;
